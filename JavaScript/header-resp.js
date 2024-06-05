290548597609724059bcebd31bbfb2ba4d23c5f7
werivik <wervik00994@stud.noroff.no>
2024-06-05 16:10:06 +0200

    making it extra responsive

---
+++ JavaScript/header-resp.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const header = document.querySelector('header');
+    const toggleIcon = document.createElement('div');
+
+    toggleIcon.classList.add('toggle-icon');
+    toggleIcon.innerHTML = '<i class="fa fa-chevron-right"></i>';
+
+    document.body.appendChild(toggleIcon);
+
+    function toggleHeader() {
+
+        if (header.classList.contains('hidden')) {
+            header.classList.remove('hidden');
+            toggleIcon.innerHTML = '<i class="fa fa-chevron-left"></i>';
+            toggleIcon.style.left = `${header.offsetWidth}px`;
+        } 
+        
+        else {
+            header.classList.add('hidden');
+            toggleIcon.innerHTML = '<i class="fa fa-chevron-right"></i>';
+            toggleIcon.style.left = '0';
+        }
+
+        const mainContent = document.querySelector('main');
+        mainContent.classList.toggle('blur');
+    }
+
+    toggleIcon.addEventListener('click', toggleHeader);
+
+    function checkScreenWidth() {
+
+        if (window.innerWidth <= 1000) {
+            header.classList.add('hidden');
+            toggleIcon.style.left = '0';
+            toggleIcon.classList.add('show');
+        } 
+        
+        else {
+            header.classList.remove('hidden');
+            toggleIcon.classList.remove('show');
+        }
+    }
+
+    window.addEventListener('resize', checkScreenWidth);
+    checkScreenWidth();
+
+    const mainContent = document.querySelector('main');
+
+    header.addEventListener('click', () => {
+        mainContent.classList.toggle('blur');
+    });
+
+});
