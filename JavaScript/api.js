fca744eac5f462bd7fb6602132610a2cc1700b0f
werivik <wervik00994@stud.noroff.no>
2024-06-11 13:17:33 +0200

    nice

---
+++ JavaScript/api.js
@@ -0,0 +1,12 @@
+"website" :{
+    "id": string,
+    "title": string,
+    "type": string,
+    "about": string,
+    "process": string,
+    "tools": string,
+    "learningpoints": string,
+    "githublink": string,
+    "netlifylink": string,
+    "image": string
+}
